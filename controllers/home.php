9fb7ac27f5de53ab6ef6180d4aac485b7a4cd6b3
Erwan Soulas <erwan.soulas@gmail.com>
2024-03-03 15:36:13 +0100

    implement Database connection and fetching of datas

--- controllers/home.php
+++ controllers/home.php
@@ -1,3 +1,8 @@
 <?php
 
-loadView('home'); 
+$confiq = require basePath('config/db.php');
+$db = new Database($confiq);
+
+$listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
+
+loadView('home', ['listings' => $listings]); 
